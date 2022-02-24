3509d70b9cf9ad6ccf2e0e75bc0dd8ebe49a9c61
WillAlejo <99842801+WillAlejo@users.noreply.github.com>
2022-02-24 03:34:10 -0500

    Add files via upload

---
+++ app/src/main/java/gradleJDK11/NewClassJDK11.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gradleJDK11;
+
+/**
+ *
+ * @author willa
+ */
+public class NewClassJDK11 {
+    public static String estasix(){
+        return "Javaaaccc";
+    }
+}
