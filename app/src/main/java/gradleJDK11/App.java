d9ebd76e0b7e1d9c362820e96706508a4a525c4e
WillAlejo <99842801+WillAlejo@users.noreply.github.com>
2022-02-25 18:36:15 -0500

    Update App.java

--- app/src/main/java/gradleJDK11/App.java
+++ app/src/main/java/gradleJDK11/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.print(NewClassJDK11.estasix());
-        System.out.print("Ahora sí");
+        String Car = "Carooo";
+        System.out.print(Car);
     }
 }
