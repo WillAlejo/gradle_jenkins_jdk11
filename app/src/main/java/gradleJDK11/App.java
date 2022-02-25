89448444065189ff43a7b8d1ca1549b72ae88a88
WillAlejo <99842801+WillAlejo@users.noreply.github.com>
2022-02-24 22:47:05 -0500

    Update App.java

--- app/src/main/java/gradleJDK11/App.java
+++ app/src/main/java/gradleJDK11/App.java
@@ -12,5 +12,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
         System.out.print(NewClassJDK11.estasix());
+        System.out.print("Ahora sí");
     }
 }
